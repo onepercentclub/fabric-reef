49a0c4a8d11422a0dc9ccee99ba2287325eb9b46
Loek van Gent <loek@1procentclub.nl>
2015-04-20 17:25:44 +0200

    Add restore db method

--- fabric_reef/db.py
+++ fabric_reef/db.py
@@ -31,3 +31,54 @@ def backup_db(db_username="reef", db_name="reef", commit=None):
     # Clearup the local database dump
     print("Removing local db dump")
     run_web("rm /tmp/{0}".format(backup_name))
+
+
+
+@roles('backup')
+@task
+def restore_db():
+    backup_dir = "/home/backups/saas-backups/saas/"
+    with cd(backup_dir):
+        output = run("ls -1t *.bz2 | head -1")
+        try:
+            filename = output.split()[0]
+        except IndexError:
+            print "No database backup file found"
+
+        if filename:
+            get(remote_path="{0}/{1}".format(backup_dir, filename), local_path="./dump.sql.bz2")
+            confirmed = confirm('Are you sure you want to replace the current database?', default=False)
+            if confirmed:
+                replace_db("./dump.sql.bz2")
+                set_tenant_domains()
+
+
+def replace_db(filename="./dump.sql.bz2", db_name="saas"):
+    local("dropdb {0}".format(db_name))
+    local("createdb {0}".format(db_name))
+    local("bunzip2 {0} -c | psql {1}".format(filename, db_name))
+
+
+def set_tenant_domains(domain_base="localhost", db_name="saas"):
+    cmd = "UPDATE clients_client SET domain_url = CONCAT(schema_name, '.{0}')".format(domain_base)
+    local('echo "{0}" | psql {1}'.format(cmd, db_name))
+
+def run_migrations():
+    run_web('./manage.py migrate_schemas --settings=%s' % env.django_settings)
+
+
+def unpack_db(filename="dump.sql.bz2"):
+    try:
+        local("bunzip2 {0}".format(filename))
+    except IndexError:
+        print "No database file found"
+
+
+@task
+def sync_media(local_static_dir="static/"):
+    """ Sync media from production backup to local. """
+    media_dir = "onepercentclub-backups/onepercentsite/media-backup/media"
+    backup_host = "backups@bluebucket.onepercentclub.com"
+    local("rsync -chavzP --stats {0}:{1} {2}".format(backup_host, media_dir, local_static_dir))
+    print("Done. Now use 'runserver --nostatic' or delete all entries in 'thumbnail_kvstore' table.")
+
